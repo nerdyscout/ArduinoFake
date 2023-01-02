127e0f5dc392609f62275d87e58428ee38bb256a
nerdyscout <nerdyscout@posteo.de>
2023-01-02 16:08:45 +0100

    adding clang format

--- src/ArduinoFake.h
+++ src/ArduinoFake.h
@@ -4,35 +4,31 @@
 #define USBCON
 #endif
 
-#include <map>
-#include <cstring>
 #include <cstdint>
+#include <cstring>
+#include <map>
 #include <stdexcept>
-#include "fakeit/fakeit.hpp"
 
-#include "arduino/Arduino.h"
-
-#include "FunctionFake.h"
-#include "StreamFake.h"
-#include "SerialFake.h"
-#include "WireFake.h"
 #include "ClientFake.h"
+#include "FunctionFake.h"
 #include "PrintFake.h"
 #include "SPIFake.h"
+#include "SerialFake.h"
+#include "StreamFake.h"
+#include "WireFake.h"
+#include "arduino/Arduino.h"
+#include "fakeit/fakeit.hpp"
 
 #define ArduinoFake(mock) _ArduinoFakeGet##mock()
 
-#define ArduinoFakeReset() \
-    getArduinoFakeContext()->reset()
+#define ArduinoFakeReset() getArduinoFakeContext()->reset()
 
 #define ArduinoFakeInstance(mock, ...) \
-    getArduinoFakeContext()->mock(__VA_ARGS__)
+  getArduinoFakeContext()->mock(__VA_ARGS__)
 
-#define ArduinoFakeMock(mock) \
-    new mock##FakeProxy(ArduinoFakeInstance(mock))
+#define ArduinoFakeMock(mock) new mock##FakeProxy(ArduinoFakeInstance(mock))
 
-#define _ArduinoFakeGetMock(mock) \
-    getArduinoFakeContext()->Mocks->mock
+#define _ArduinoFakeGetMock(mock) getArduinoFakeContext()->Mocks->mock
 
 #define _ArduinoFakeGet() _ArduinoFakeGetMock(Function)
 #define _ArduinoFakeGetFunction() _ArduinoFakeGetMock(Function)
@@ -41,97 +37,85 @@
 #define _ArduinoFakeGetStream() _ArduinoFakeGetMock(Stream)
 #define _ArduinoFakeGetClient() _ArduinoFakeGetMock(Client)
 #define _ArduinoFakeGetPrint() _ArduinoFakeGetMock(Print)
-#define _ArduinoFakeGetSPI() _ArduinoFakeGetMock(SPI)
+#define _ArduinoFakeGetSPIBus() _ArduinoFakeGetMock(SPIBus)
 
-#define _ArduinoFakeInstanceGetter1(mock)                     \
-    mock##Fake *mock()                                        \
-    {                                                         \
-        if (!this->Instances->mock)                           \
-        {                                                     \
-            this->Instances->mock = &this->Mocks->mock.get(); \
-        }                                                     \
-        return this->Instances->mock;                         \
-    }
+#define _ArduinoFakeInstanceGetter1(mock)               \
+  mock##Fake *mock() {                                  \
+    if (!this->Instances->mock) {                       \
+      this->Instances->mock = &this->Mocks->mock.get(); \
+    }                                                   \
+    return this->Instances->mock;                       \
+  }
 
-#define _ArduinoFakeInstanceGetter2(name, clazz)                                 \
-    name##Fake *name(class clazz *instance)                                      \
-    {                                                                            \
-        if (Mapping[instance])                                                   \
-        {                                                                        \
-            return (name##Fake *)Mapping[instance];                              \
-        }                                                                        \
-        if (dynamic_cast<name##FakeProxy *>(instance))                           \
-        {                                                                        \
-            return dynamic_cast<name##FakeProxy *>(instance)->get##name##Fake(); \
-        }                                                                        \
-        throw std::runtime_error("Unknown instance");                            \
-    }
+#define _ArduinoFakeInstanceGetter2(name, clazz)                           \
+  name##Fake *name(class clazz *instance) {                                \
+    if (Mapping[instance]) {                                               \
+      return (name##Fake *)Mapping[instance];                              \
+    }                                                                      \
+    if (dynamic_cast<name##FakeProxy *>(instance)) {                       \
+      return dynamic_cast<name##FakeProxy *>(instance)->get##name##Fake(); \
+    }                                                                      \
+    throw std::runtime_error("Unknown instance");                          \
+  }
 
-struct ArduinoFakeMocks
-{
-    fakeit::Mock<FunctionFake> Function;
-    fakeit::Mock<SerialFake> Serial;
-    fakeit::Mock<WireFake> Wire;
-    fakeit::Mock<StreamFake> Stream;
-    fakeit::Mock<ClientFake> Client;
-    fakeit::Mock<PrintFake> Print;
-    fakeit::Mock<SPIBusFake> SPIBus;
+struct ArduinoFakeMocks {
+  fakeit::Mock<FunctionFake> Function;
+  fakeit::Mock<SerialFake> Serial;
+  fakeit::Mock<WireFake> Wire;
+  fakeit::Mock<StreamFake> Stream;
+  fakeit::Mock<ClientFake> Client;
+  fakeit::Mock<PrintFake> Print;
+  fakeit::Mock<SPIBusFake> SPIBus;
 };
 
-struct ArduinoFakeInstances
-{
-    FunctionFake *Function;
-    SerialFake *Serial;
-    WireFake *Wire;
-    StreamFake *Stream;
-    ClientFake *Client;
-    PrintFake *Print;
-    SPIBusFake *SPIBus;
+struct ArduinoFakeInstances {
+  FunctionFake *Function;
+  SerialFake *Serial;
+  WireFake *Wire;
+  StreamFake *Stream;
+  ClientFake *Client;
+  PrintFake *Print;
+  SPIBusFake *SPIBus;
 };
 
-class ArduinoFakeContext
-{
-public:
-    ArduinoFakeInstances *Instances = new ArduinoFakeInstances();
-    ArduinoFakeMocks *Mocks = new ArduinoFakeMocks();
-    std::map<void *, void *> Mapping;
+class ArduinoFakeContext {
+ public:
+  ArduinoFakeInstances *Instances = new ArduinoFakeInstances();
+  ArduinoFakeMocks *Mocks = new ArduinoFakeMocks();
+  std::map<void *, void *> Mapping;
 
-    _ArduinoFakeInstanceGetter1(Function)
-        _ArduinoFakeInstanceGetter1(Serial)
-            _ArduinoFakeInstanceGetter1(Wire)
-                _ArduinoFakeInstanceGetter1(Stream)
-                    _ArduinoFakeInstanceGetter1(Client)
-                        _ArduinoFakeInstanceGetter1(Print)
-                            _ArduinoFakeInstanceGetter1(SPIBus)
+  _ArduinoFakeInstanceGetter1(Function);
+  _ArduinoFakeInstanceGetter1(Serial);
+  _ArduinoFakeInstanceGetter1(Wire);
+  _ArduinoFakeInstanceGetter1(Stream);
+  _ArduinoFakeInstanceGetter1(Client);
+  _ArduinoFakeInstanceGetter1(Print);
+  _ArduinoFakeInstanceGetter1(SPIBus);
 
-                                _ArduinoFakeInstanceGetter2(Serial, Serial_)
-                                    _ArduinoFakeInstanceGetter2(Wire, TwoWire)
-                                        _ArduinoFakeInstanceGetter2(Stream, Stream)
-                                            _ArduinoFakeInstanceGetter2(Client, Client)
-                                                _ArduinoFakeInstanceGetter2(Print, Print)
-        //                                                    _ArduinoFakeInstanceGetter2(SPIBus, SPIClass)
+  _ArduinoFakeInstanceGetter2(Serial, Serial_);
+  _ArduinoFakeInstanceGetter2(Wire, TwoWire);
+  _ArduinoFakeInstanceGetter2(Stream, Stream);
+  _ArduinoFakeInstanceGetter2(Client, Client);
+  _ArduinoFakeInstanceGetter2(Print, Print);
+  //  _ArduinoFakeInstanceGetter2(SPIBus, SPIBus);
 
-        ArduinoFakeContext()
-    {
-        this->reset();
-    }
+  ArduinoFakeContext() { this->reset(); }
 
-    void reset(void)
-    {
-        this->Instances = new ArduinoFakeInstances();
+  void reset(void) {
+    this->Instances = new ArduinoFakeInstances();
 
-        this->Mocks->Function.Reset();
-        this->Mocks->Stream.Reset();
-        this->Mocks->Serial.Reset();
-        this->Mocks->Wire.Reset();
-        this->Mocks->Client.Reset();
-        this->Mocks->Print.Reset();
-        this->Mocks->SPIBus.Reset();
+    this->Mocks->Function.Reset();
+    this->Mocks->Stream.Reset();
+    this->Mocks->Serial.Reset();
+    this->Mocks->Wire.Reset();
+    this->Mocks->Client.Reset();
+    this->Mocks->Print.Reset();
+    this->Mocks->SPIBus.Reset();
 
-        Mapping[&::Serial] = this->Serial();
-        Mapping[&::Wire] = this->Wire();
-        Mapping[&::SPI] = this->SPIBus();
-    }
+    Mapping[&::Serial] = this->Serial();
+    Mapping[&::Wire] = this->Wire();
+    Mapping[&::SPI] = this->SPIBus();
+  }
 };
 
 ArduinoFakeContext *getArduinoFakeContext();
